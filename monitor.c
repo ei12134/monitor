99ba88c77ae3afeaf6aecb4d8bf3aaa94322b1a6
ei12134 <ei12134@fe.up.pt>
2014-04-04 16:39:36 +0100

    file checker

--- monitor.c
+++ monitor.c
@@ -22,6 +22,7 @@
 #define TAIL "/usr/bin/tail"
 #define GREP "/bin/grep"
 #define STAT "/usr/bin/stat"
+#define DATE "/bin/date"
 
 void sig_pipe(int signo);
 void sig_alarm(int signo);
@@ -51,21 +52,12 @@ int main(int argc, char *argv[]) {
   // Create two arrays containing file descriptors needed to establish pipe connections
   int fd1[2]; // pipe connection between tail & grep (both separate monitor childs)
   int fd2[2]; // pipe connection between grep and monitor
-  int n;
-  pid_t childpid1, childpid2;
+  int file, n;
+  pid_t childpid1, childpid2, childpid3;
   char line[MAXLINE];
-
-  //  execl("stat", "stat", "--printf='%y'", "ler", (char*) 0);
-  //  execl("find", "find", ".", "-name ler -printf '%TY-%Tm-%TdT%TH:%TM:%TS'", (char*) 0);
 
   // sig_pipe handler
   signal(SIGPIPE, sig_pipe);
-
-  /* time_t t = time(NULL); */
-  /* struct tm tm = *localtime(&t); */
-    
-  /* printf("%d-%d-%d %d:%d:%d - %s.txt - ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, argv[3]); 
-   fflush(stdout); */
 
   // First create pipes
   if (pipe (fd1) == -1)
@@ -90,62 +82,82 @@ int main(int argc, char *argv[]) {
     dup2(fd2[READ],STDIN_FILENO);
     
     while (fgets(line, MAXLINE,stdin) != NULL) {
-	n=strlen(line);
-	write(fd1[WRITE],line,n);
-	n=read(fd2[READ],line,MAXLINE); // waits for grep output
-	if (n==0) {
-	    perror("child closed pipe");
-	    break;
-	} 
-	line[n]=0; // null ending char is not received, so "add" it
-	
-	fflush(stdout);
-	printf("%s.txt - ", argv[3]);
-	printf("%s", line);
+      n=strlen(line);
+      write(fd1[WRITE],line,n);
+      n=read(fd2[READ],line,MAXLINE); // waits for grep output
+      if (n==0) {
+	perror("child closed pipe");
+	break;
+      } 
+      line[n]=0; // null ending char is not received, so "add" it
+
+
+      char buff[100];
+      time_t now = time (0);
+
+      strftime ( buff, 100, "%Y-%m-%dT%H:%M:%S", localtime (&now));
+      printf ("%s - %s.txt - %s", buff,  argv[3], line);
     }    
     wait(&status);  
   }
-  /* Child1 code */
-  if (childpid1 == 0) {
-    printf("PID of tail child = %d; PPID = %d\n", getpid(), getppid());
-    // Create child2 responsible for executing grep command
-    childpid2 = fork();
-      
-    if (childpid2 == -1){
-      perror("Failed to fork");
-      exit(EXIT_FAILURE);
-    }
-	
-    if (childpid2 > 0){
-      // Redirect output through pipe 1 for grep
-      close(fd1[READ]);
-      if (fd1[WRITE] != STDOUT_FILENO) {
-	if (dup2(fd1[WRITE],STDOUT_FILENO) != STDOUT_FILENO)
-	  perror("dup1 error to stdout in pipe 1");
-      }
-      close(fd1[WRITE]);
-      
-      execl(TAIL,"tail","-n 0", "-f" , argv[3], (char*) 0 );
-      perror("tail execl error");
-    }
-    
-    /* Child2 code */
-    else if (childpid2 == 0) {
-      printf("PID of grep child = %d; PPID = %d\n", getpid(), getppid());
-      // Receive tail input redirected through pipe 1
-      close(fd1[WRITE]);
-      dup2(fd1[READ],STDIN_FILENO);
 
-      // redirect grep output to parent
-      close(fd2[READ]);
-      if (dup2(fd2[WRITE],STDOUT_FILENO) != STDOUT_FILENO)
-	perror("dup2 error to stdout in pipe 2");
-      close(fd2[WRITE]);
-     
-      execl(GREP, "grep", "--line-buffered", "-w", argv[2], (char*) 0);
-      perror("grep execl error");
+  // Child1 code - file checker
+  if (childpid1 == 0){
+    childpid2 = fork();
+    if (childpid2 > 0){
+      while (1){
+	sleep(5);
+	file = open(argv[3], O_RDONLY);
+	if (file == -1) {
+	  perror(argv[3]);
+	  printf("file does not exist anymore");
+	}
+      }
     }
-  } 
+    if (childpid2 == 0){
+      /* Child2 code - tail */
+      if (childpid2 == 0) {
+	printf("PID of tail child = %d; PPID = %d\n", getpid(), getppid());
+	// Create child3 responsible for executing grep command
+	childpid3 = fork();
+	
+	if (childpid3 == -1){
+	  perror("Failed to fork");
+	  exit(EXIT_FAILURE);
+	}
+	
+	if (childpid3 > 0){
+	  // Redirect output through pipe 1 for grep
+	  close(fd1[READ]);
+	  if (fd1[WRITE] != STDOUT_FILENO) {
+	    if (dup2(fd1[WRITE],STDOUT_FILENO) != STDOUT_FILENO)
+	      perror("dup1 error to stdout in pipe 1");
+	  }
+	  close(fd1[WRITE]);
+      
+	  execl(TAIL,"tail","-n 0", "-f" , argv[3], (char*) 0 );
+	  perror("tail execl error");
+	}
+    
+	/* Child3 code  - grep */
+	else if (childpid3 == 0) {
+	  printf("PID of grep child = %d; PPID = %d\n", getpid(), getppid());
+	  // Receive tail input redirected through pipe 1
+	  close(fd1[WRITE]);
+	  dup2(fd1[READ],STDIN_FILENO);
+
+	  // redirect grep output to parent
+	  close(fd2[READ]);
+	  if (dup2(fd2[WRITE],STDOUT_FILENO) != STDOUT_FILENO)
+	    perror("dup2 error to stdout in pipe 2");
+	  close(fd2[WRITE]);
+     
+	  execl(GREP, "grep", "--line-buffered", "-w", argv[2], (char*) 0);
+	  perror("grep execl error");
+	}
+      } 
+    }
+  }
   exit(EXIT_SUCCESS);
 }
 
@@ -158,7 +170,7 @@ void sig_alarm(int signo) {
 #ifdef DEBUG
   printf("SIGALARM caught\n");
 #endif
-  printf("Monitor user set timer expired\n");
+  printf("User set timer expired - monitor stopped\n");
   kill(-parent_pid, SIGQUIT);
   exit(EXIT_SUCCESS);
 }
@@ -175,8 +187,7 @@ long int parse_long(char *str, int base) {
   char *endptr;
   long int timer = strtol(str, &endptr, base);
 
-  /* Check for various possible errors */
-  
+  /* Check for various possible errors */  
   if ((errno == ERANGE && (timer == LONG_MAX || timer == LONG_MIN)) || (errno != 0 && timer == 0)) {
     perror("strtol");
     return LONG_MAX;
